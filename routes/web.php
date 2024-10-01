fe391bec9e146b2dbf5a576ae07edd1199858641
jeovane93 <fariajeovane209@gmail.com>
2024-10-01 14:47:24 +0000

    atulização web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CarrinhoController;
 use App\Http\Controllers\SiteController;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SiteController::class, 'index'])->name('site.home');
